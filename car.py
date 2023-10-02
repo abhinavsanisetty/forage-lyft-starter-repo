5627c02d6db0bc7bfeec759934d4296f9dfe6b1f
abhinavsanisetty <122497191+abhinavsanisetty@users.noreply.github.com>
2023-10-01 21:13:56 -0700

    Add files via upload

---
+++ car.py
@@ -0,0 +1,12 @@
+from abc import ABC
+from servicable import Serviceable
+
+class Car(Servicable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
+
+
